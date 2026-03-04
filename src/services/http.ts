8450c3b521f80486618c3e14b6298c708678613c
Resty <restypabello@gmail.com>
2026-03-04 15:57:05 +0800

    Update http.ts to support axios config and data parameters

--- src/services/http.ts
+++ src/services/http.ts
@@ -1,23 +1,20 @@
 import { api } from './api';
 
 export const http = {
-    get: async <T>(url: string) => {
-        const response = await api.get<T>(url);
+    get: async <T>(url: string, config?: any) => {
+        const response = await api.get<T>(url, config);
         return response.data;
     },
-
-    post: async <T>(url: string, data: any) => {
-        const response = await api.post<T>(url, data);
+    post: async <T>(url: string, data?: any, config?: any) => {
+        const response = await api.post<T>(url, data, config);
         return response.data;
     },
-
-    put: async <T>(url: string, data: any) => {
-        const response = await api.put<T>(url, data);
+    put: async <T>(url: string, data?: any, config?: any) => {
+        const response = await api.put<T>(url, data, config);
         return response.data;
     },
-
-    delete: async <T>(url: string) => {
-        const response = await api.delete<T>(url);
+    delete: async <T>(url: string, config?: any) => {
+        const response = await api.delete<T>(url, config);
         return response.data;
-    },
+    }
 };
