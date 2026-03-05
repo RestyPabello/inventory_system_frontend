090586cc53886b64a467f27d6767c5069d6c9cdb
Resty <restypabello@gmail.com>
2026-03-05 16:56:18 +0800

    Add loading template

---
+++ src/views/Template/Loading.vue
@@ -0,0 +1,5 @@
+<template>
+    <div class="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 z-50">
+        <span class="loading loading-dots loading-xl text-gray-800 dark:text-white/90"></span>
+    </div>
+</template>
