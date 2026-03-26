c17bd92b289a69059eefd001376e1be113cfd201
Resty <restypabello@gmail.com>
2026-03-26 16:21:08 +0800

    feat: implement item creation with image upload support

--- src/services/itemService.ts
+++ src/services/itemService.ts
@@ -1,5 +1,5 @@
 import { http } from './http';
-import type { FrontendItem } from '@/types/frontend/FrontendItem';
+import type {  FrontendItem, CreateItemRequest } from '@/types/frontend/FrontendItem';
 
 export const getItems = async (search: string): Promise<FrontendItem[]> => {
     
@@ -20,3 +20,43 @@ export const getItems = async (search: string): Promise<FrontendItem[]> => {
         purchased_at: data.purchased_at ?? "",
     }));
 };
+
+export const addItem = async (data: CreateItemRequest): Promise<any> => {
+    const fd = new FormData();
+
+    fd.append('name', data.name);
+    fd.append('brand', data.brand);
+    fd.append('price', String(data.price ?? 0));
+    fd.append('category_id', String(data.category_id)); 
+    fd.append('unit_id', String(data.unit_id));    
+    fd.append('value', String(data.value ?? 0));
+    fd.append('quantity', String(data.quantity ?? 0));
+    fd.append('status', data.status ?? '');
+    fd.append('expires_at', data.expires_at ?? '');
+    fd.append('purchased_at', data.purchased_at ?? '');
+    fd.append('item_description', data.description ?? '')
+    
+    if (data.image instanceof File) {
+        fd.append('image', data.image);
+    }
+
+    const result = await http.post<any>('/items', fd);
+
+    const variant = result.data.variants?.[0] || {};
+    const stock   = variant.stocks?.[0] || {};
+
+    return {
+        id: result.data.id,
+        name: result.data.name,
+        brand: result.data.brand, 
+        category_id: result.data.category_id,   
+        unit_id: result.data.unit_id,
+        image: variant.image_url ?? null,
+        description: result.data.description ?? null,
+        price: Number(variant.price) || 0,
+        status: stock.status ?? null,
+        quantity: Number(stock.quantity) || 0,
+        expires_at: stock.expires_at ?? "",
+        purchased_at: stock.purchased_at ?? "",
+    };
+}
