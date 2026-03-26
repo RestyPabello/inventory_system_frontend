1a9d27227dba98f79311d7c30518d176522e9cee
Resty <restypabello@gmail.com>
2026-03-26 16:28:31 +0800

    style: add custom dark mode styles for flatpickr

--- src/components/ui/BaseInput.vue
+++ src/components/ui/BaseInput.vue
@@ -70,3 +70,16 @@
         altInputClass: 'cursor-pointer dark:bg-dark-900 h-11 w-full rounded-lg border border-gray-300 bg-transparent px-4 py-2.5 pr-10 text-sm text-gray-800 shadow-theme-xs placeholder:text-gray-400 focus:border-brand-300 focus:outline-hidden focus:ring-3 focus:ring-brand-500/10 dark:border-gray-700 dark:bg-gray-900 dark:text-white/90 dark:placeholder:text-white/30 dark:focus:border-brand-800 transition-all',
     };
 </script>
+
+<style>
+
+.dark .flatpickr-monthDropdown-months,
+.dark .flatpickr-monthDropdown-month {
+    background-color: #101828 !important; 
+    color: #ffffff !important;
+}
+
+.flatpickr-monthDropdown-months:focus {
+    outline: none !important;
+}
+</style>
